3a08de5c933baefd0323facc36c417130645acdc
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2020-01-20 14:05:03 +0300

    init

---
+++ app/src/main/java/vip/yazilim/p2g/android/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package vip.yazilim.p2g.android.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/vip/yazilim/p2g/android/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package vip.yazilim.p2g.android.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
